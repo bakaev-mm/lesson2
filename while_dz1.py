aa402caf028c15f4d157628a76d2bed4eb927785
Mikhail Bakaev <homermen@mail.ru>
2017-11-21 22:53:35 +0300

    dz while

---
+++ while_dz1.py
@@ -0,0 +1,6 @@
+name_list = ['Вася', 'Маша', 'Петя', 'Валера', 'Саша', 'Даша']
+while True:
+    name = name_list.pop()
+    if name == 'Валера':
+        print('Валера нашелся')
+        break
---
+++ while_dz2.py
@@ -0,0 +1,9 @@
+def find_person(name):
+    while True:
+       if  find_name == name_list.pop():
+            print('{} нашелся'.format(find_name))
+            break
+
+find_name = 'Вася'
+name_list = ['Вася', 'Маша', 'Петя', 'Валера', 'Саша', 'Даша']
+find_person(find_name)
