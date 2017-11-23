e42be49413a1a68eedc49cd8468ad7f8c2d26645
Bakaev-MM <bakaev-mm@gamma-center.ru>
2017-11-23 17:39:38 +0300

    dz_exception

--- while_dz3.py
+++ while_dz3.py
@@ -1,7 +1,10 @@
 def ask_user(): 
-    while True:
-        user_say = input('Как дела?')
-        if user_say == 'Хорошо':
-            break
-
+    try:
+        while True:
+            user_say = input('Как дела?')
+            if user_say == 'Хорошо':
+                break
+    except (KeyboardInterrupt):
+        print('Пока!')
+    
 ask_user()
